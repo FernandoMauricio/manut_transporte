c3efb1efaa4d0ef0f3a9c97dbcc41f5844e36759
Fernando Mauricio <fernando.mauricio@am.senac.br>
2018-10-05 10:34:28 -0400

    Alteração na impressão do dia

--- views/transporte-admin-acompanhamento/imprimir.php
+++ views/transporte-admin-acompanhamento/imprimir.php
@@ -4,7 +4,13 @@ use yii\helpers\Html;
 <style type="text/css">
 	.table>tbody>tr>td, .table>tbody>tr>th, .table>tfoot>tr>td, .table>tfoot>tr>th, .table>thead>tr>td, .table>thead>tr>th 
 	{
-		font-size: 12px;
+		font-size: 10px;
+	}
+
+	.table-bordered>tbody>tr>td, .table-bordered>tbody>tr>th, .table-bordered>tfoot>tr>td, .table-bordered>tfoot>tr>th, .table-bordered>thead>tr>td, .table-bordered>thead>tr>th
+	{
+		vertical-align: inherit;
+		text-align: center;
 	}
 </style>
 
@@ -17,62 +23,73 @@ use yii\helpers\Html;
 </head>
 
 <body>
-	<div>
-	  	<h3>Histórico do Percurso da Solicitação de Transporte</h3>
-	  	<table class="table">
-	    	<thead>
-	      	<tr>
-	      		<th>Unidade</th>
-	      		<th>Solicitante</th>
-	        	<th>Destino</th>
-	        	<th>Bairro</th>
-	        	<th>Hora</th>
-	        	<th>Observação</th>
-	      	</tr>
-	    	</thead>
-<?php foreach ($models as $i => $model): ?>
-	    <tbody>
-	      	<tr>
-	        	<td class="col-sm-2"><?= $model->unidade_solic ?></td>
-	        	<td class="col-sm-1"><?= $model->usuario_solic_nome ?></td>
-	        	<td class="col-sm-2"><?= $model->local ?></td>
-	        	<td class="col-sm-1"><?= $model->bairro->descricao ?></td>
-	        	<td class="col-sm-1"><?=  date("H:i",strtotime($model->hora_confirmacao)) ?></td>
-	        	<td class="col-sm-3"></td>
-<!-- 	        	<pre>
-	        	<?= print_r($model);?> -->
-	      	</tr>
-	    </tbody>
-<?php endforeach; ?>
-	  	</table>
-	</div>
+	<table class="table table-condensed table-hover">
+		<tr> 
+			<td style="border-top: 0px solid" width="50%"><h3>Histórico do Percurso da Solicitação de Transporte</h3></td>
+			<td style="border-top: 0px solid" width="40%"><img width="20%" src="css/img/logo.png"></td>
+		</tr>
+	</table>
 
-	<div>
-	  	<table class="table table-bordered">
-	    	<thead>
-	      	<tr>
-	        	<th class="col-sm-3">Local</th>
-	        	<th>Data</th>
-	        	<th>Horário</th>
-	        	<th>Odômetro</th>
-	        	<th>Diferença</th>
-	        	<th>Quem Utilizou</th>
-	        	<th class="col-sm-3">Intinerário</th>
+	<table class="table">
+		<thead>
+	  	<tr>
+	  		<th>Unidade</th>
+	  		<th>Solicitante</th>
+	    	<th>Destino</th>
+	    	<th>Bairro</th>
+	    	<th>Hora</th>
+	    	<th>Observação</th>
+	  	</tr>
+		</thead>
+		<tbody>
+			<?php foreach ($models as $i => $model): ?>
+		  	<tr>
+		    	<td class="col-sm-2"><?= $model->unidade_solic ?></td>
+		    	<td class="col-sm-1"><?= $model->usuario_solic_nome ?></td>
+		    	<td class="col-sm-2"><?= $model->local ?></td>
+		    	<td class="col-sm-1"><?= $model->bairro->descricao ?></td>
+		    	<td class="col-sm-1"><?=  date("H:i",strtotime($model->hora_confirmacao)) ?></td>
+		    	<td class="col-sm-3"></td>
+		  	</tr>
+			<?php endforeach; ?>
+		</tbody>
+	</table>
+
+	<table class="table table-bordered">
+	  	<tr>
+	    	<th style="vertical-align:inherit;" rowspan="2">Data</th>
+	    	<th style="vertical-align:inherit;" colspan="2">Horário</th>
+	    	<th style="vertical-align:inherit;" colspan="2">Odômetro</th>
+	    	<th style="vertical-align:inherit;" rowspan="2">Diferença</th>
+	    	<th style="vertical-align:inherit;" rowspan="2">Quem Utilizou</th>
+	    	<th style="vertical-align:inherit;" rowspan="2" class="col-sm-3">Intinerário</th>
+	  	</tr>
+	    	<tr>
+	      		<td>Saída</td>
+	      		<td>Regresso</td>
+	      		<td>Saída</td>
+	      		<td>Regresso</td>
+	    	</tr>
+	    <?php for ($row = 0; $row < 7; $row ++): ?>
+	      	<tr style="height: 30px;">
+	      		<td></td>
+	      		<td></td>
+	      		<td></td>
+	      		<td></td>
+	      		<td></td>
+	      		<td></td>
+	      		<td></td>
+	      		<td></td>
 	      	</tr>
-	    	</thead>
-		    <tbody>
-		    <?php for ($row = 0; $row < 7; $row ++) { ?>
-		      	<tr style="height: 30px;">
-		      		<td></td>
-		      		<td></td>
-		      		<td></td>
-		      		<td></td>
-		      		<td></td>
-		      		<td></td>
-		      		<td></td>
-		      	</tr>
-		    <?php } ?>
-		    </tbody>
-	  	</table>
-	</div>
+	    <?php endfor; ?>
+	</table>
+
+	<table class="table table-condensed">
+		<tbody>
+			<tr>
+		  		<td style="border-top: 0px solid">Assinatura do Motorista: _______________________ _______/_______/______</td>
+		  		<td style="border-top: 0px solid">Visto GMT: _______________________ _______/_______/______</td>
+		  	</tr>
+		</tbody>
+	</table>
 </body>
